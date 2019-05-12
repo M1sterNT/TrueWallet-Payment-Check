27b18c6fb9bfe98b2beadcac76c45899aacebc09
MrTheBank <thebankzana@yahoo.com>
2019-05-12 12:06:31 +0700

    Update tw_transactions.sql

--- tw_transactions.sql
+++ tw_transactions.sql
@@ -1,59 +1,26 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Apr 05, 2019 at 10:30 AM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.1.27
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `maxnusco_donation`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tw_transactions`
---
-
 CREATE TABLE `tw_transactions` (
   `id` int(11) NOT NULL,
   `name` varchar(50) DEFAULT NULL,
   `txn_id` varchar(50) DEFAULT NULL,
+  `type` varchar(50) DEFAULT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `tw_transactions`
---
 ALTER TABLE `tw_transactions`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tw_transactions`
---
 ALTER TABLE `tw_transactions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
